eaadfb9d65919088e466cae603018ae04f97d75d
bebroch <cawapog@yandex.ru>
2024-05-09 19:33:55 +0300

    fix: bottom

--- src/bottom/bottom.tsx
+++ src/bottom/bottom.tsx
@@ -1,6 +1,6 @@
 function Bottom() {
     return (
-        <div className="container mx-auto mb-3 px-3 md:px-0">
+        <div className=" mx-auto mb-3 px-3 lg:px-0 lg:container">
             <div className="bg-yellow-600 rounded-lg shadow-lg h-16">
                 <div className="flex items-center justify-center h-full">
                     <div className="text-white">© 2024 Chris Trees. All Rights Reserved.</div>
